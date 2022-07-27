cda9c2184b2f2edb79bca3bf31a5985a2489c64f
goyangrui <oyanggeorge@gmail.com>
2022-07-27 19:46:37 -0400

    Landing page completed

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,36 @@
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+
+function Landing() {
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I'm baby normcore glossier chicharrones yes plz narwhal godard
+            snackwave tumeric crucifix hashtag helvetica vaporware bushwick banh
+            mi. Trust fund kogi cred af bitters deep v retro craft beer shaman
+            microdosing migas keytar occupy plaid quinoa. Williamsburg pok pok
+            hexagon artisan air plant butcher roof party tattooed. Etsy
+            church-key raw denim brooklyn. Affogato tilde la croix, YOLO tonx
+            salvia portland. Chicharrones yuccie iPhone freegan put a bird on it
+            actually 8-bit enamel pin williamsburg fashion axe brooklyn artisan
+            echo park.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+}
+
+export default Landing;
