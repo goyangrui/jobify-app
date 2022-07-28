217b1278dffa0378042fdd90b38d00220b8c259b
goyangrui <oyanggeorge@gmail.com>
2022-07-28 13:49:31 -0400

    Register page completed

---
+++ client/src/components/FormRow.js
@@ -0,0 +1,19 @@
+function FormRow(props) {
+  const { type, name, value, handleChange, labelText } = props;
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {labelText || name}
+      </label>
+      <input
+        type={type}
+        value={value}
+        name={name}
+        onChange={handleChange}
+        className="form-input"
+      />
+    </div>
+  );
+}
+
+export default FormRow;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,85 @@
+import { useState, useEffect } from "react";
+import { Logo, FormRow, Alert } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+  showAlert: false,
+};
+
 function Register() {
-  return <h1>Register</h1>;
+  const [values, setValues] = useState(initialState);
+  // global state and useNavigate
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
+  // handle input field changes
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  // handle form submissions
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+
+        {/* conditional header */}
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+
+        {/* alert */}
+        {values.showAlert && <Alert />}
+
+        {/* name input (conditional)*/}
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+
+        {/* email input */}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password input */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+
+        {/* submit button */}
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+
+        {/* conditional paragraph */}
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
 }
 
 export default Register;
