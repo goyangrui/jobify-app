ff4aa5b022fefc6608199cc34c6978dd737b7ce8
goyangrui <oyanggeorge@gmail.com>
2022-07-31 17:44:35 -0400

    completed add job and get all jobs functionality on front and back end

---
+++ client/src/components/SearchContainer.js
@@ -0,0 +1,5 @@
+function SearchContainer() {
+  return <h1>SearchContainer</h1>;
+}
+
+export default SearchContainer;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,5 +4,18 @@ import Alert from "./Alert";
 import Navbar from "./Navbar";
 import BigSidebar from "./BigSidebar";
 import SmallSidebar from "./SmallSidebar";
+import FormRowSelect from "./FormRowSelect";
+import SearchContainer from "./SearchContainer";
+import JobsContainer from "./JobsContainer";
 
-export { Logo, FormRow, Alert, Navbar, BigSidebar, SmallSidebar };
+export {
+  Logo,
+  FormRow,
+  Alert,
+  Navbar,
+  BigSidebar,
+  SmallSidebar,
+  FormRowSelect,
+  SearchContainer,
+  JobsContainer,
+};
